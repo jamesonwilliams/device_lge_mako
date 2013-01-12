5063c89efd1258a6ad043e0883d87b2859439228
Jameson Williams <jameson.h.williams@intel.com>
2013-01-12 02:46:43 -0800

    Build kernel from source, if source is present.
    Signed-off-by: Jameson Williams <jameson.h.williams@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -14,6 +14,18 @@
 # limitations under the License.
 #
 
+KERNEL_DEFCONFIG := mako_defconfig
+KERNEL_DIR := kernel/lge/mako
+
+-include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_PREBUILT_KERNEL := device/lge/mako-kernel/kernel
 endif
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+
